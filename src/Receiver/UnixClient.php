fc3c9011ab9e2cf396429b2ad694eca9a3bef4d0
Baptiste Langlade <baptouuuu@gmail.com>
2019-02-15 15:09:49 +0100

    receive messages from a specific process

---
+++ src/Receiver/UnixClient.php
@@ -0,0 +1,79 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\IPC\Receiver;
+
+use Innmind\IPC\{
+    Receiver,
+    Protocol,
+    Process,
+    Exception\NoMessage,
+};
+use Innmind\OperatingSystem\Sockets;
+use Innmind\Socket\Address\Unix as Address;
+use Innmind\Stream\Select;
+use Innmind\TimeContinuum\{
+    ElapsedPeriodInterface,
+    ElapsedPeriod,
+};
+
+final class UnixClient implements Receiver
+{
+    private $sockets;
+    private $protocol;
+    private $process;
+    private $address;
+    private $timeout;
+
+    public function __construct(
+        Sockets $sockets,
+        Protocol $protocol,
+        Process $process,
+        Address $address,
+        ElapsedPeriodInterface $timeout = null
+    ) {
+        $this->sockets = $sockets;
+        $this->protocol = $protocol;
+        $this->process = $process;
+        $this->address = $address;
+        $this->timeout = new ElapsedPeriod(
+            ($timeout ?? new ElapsedPeriod(60000))->milliseconds() // default to 1 minute
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __invoke(callable $listen): void
+    {
+        try {
+            $this->loop($listen);
+        } catch (NoMessage $e) {
+            // the other process closed the connection
+            return;
+        }
+    }
+    private function loop(callable $listen): void
+    {
+        $client = $this->sockets->connectTo($this->address);
+        $select = (new Select($this->timeout))->forRead($client);
+
+        do {
+            $sockets = $select();
+
+            try {
+                if ($sockets->get('read')->contains($client)) {
+                    $message = $this->protocol->decode($client);
+
+                    $listen($message, $this->process);
+                }
+            } catch (\Throwable $e) {
+                $client->close();
+                $select->unwatch($client);
+
+                throw $e;
+            }
+
+        } while (true);
+    }
+}
---
+++ tests/Receiver/UnixClientTest.php
@@ -0,0 +1,116 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\IPC\Receiver;
+
+use Innmind\IPC\{
+    Receiver\UnixClient,
+    Receiver,
+    Protocol,
+    Process,
+    Message,
+    Exception\NoMessage,
+};
+use Innmind\OperatingSystem\Sockets;
+use Innmind\Socket\{
+    Address\Unix as Address,
+    Client,
+};
+use PHPUnit\Framework\TestCase;
+
+class UnixClientTest extends TestCase
+{
+    public function testInterface()
+    {
+        $this->assertInstanceOf(
+            Receiver::class,
+            new UnixClient(
+                $this->createMock(Sockets::class),
+                $this->createMock(Protocol::class),
+                $this->createMock(Process::class),
+                new Address('/tmp/foo')
+            )
+        );
+    }
+
+    public function testLoop()
+    {
+        $receive = new UnixClient(
+            $sockets = $this->createMock(Sockets::class),
+            $protocol = $this->createMock(Protocol::class),
+            $process = $this->createMock(Process::class),
+            $address = new Address('/tmp/foo')
+        );
+        $client = $this->createMock(Client::class);
+        $client
+            ->expects($this->any())
+            ->method('resource')
+            ->willReturn(\tmpfile());
+        $client
+            ->expects($this->once())
+            ->method('close');
+        $sockets
+            ->expects($this->once())
+            ->method('connectTo')
+            ->with($address)
+            ->willReturn($client);
+        $protocol
+            ->expects($this->exactly(4))
+            ->method('decode')
+            ->with($client)
+            ->willReturn($message = $this->createMock(Message::class));
+
+        try {
+            $count = 0;
+
+            $receive(function($a, $b) use ($message, $process, &$count): void {
+                if ($count === 3) {
+                    throw new \Exception;
+                }
+
+                $this->assertSame($message, $a);
+                $this->assertSame($process, $b);
+                ++$count;
+            });
+
+            $this->fail('it should throw');
+        } catch (\Exception $e) {
+            $this->assertSame(3, $count);
+        }
+    }
+
+    public function testStopWhenConnectionClosedByOtherProcess()
+    {
+        $receive = new UnixClient(
+            $sockets = $this->createMock(Sockets::class),
+            $protocol = $this->createMock(Protocol::class),
+            $this->createMock(Process::class),
+            $address = new Address('/tmp/foo')
+        );
+        $client = $this->createMock(Client::class);
+        $client
+            ->expects($this->any())
+            ->method('resource')
+            ->willReturn(\tmpfile());
+        $client
+            ->expects($this->once())
+            ->method('close');
+        $sockets
+            ->expects($this->once())
+            ->method('connectTo')
+            ->with($address)
+            ->willReturn($client);
+        $protocol
+            ->expects($this->at(3))
+            ->method('decode')
+            ->with($client)
+            ->will($this->throwException(new NoMessage));
+
+        $count = 0;
+
+        $this->assertNull($receive(function() use (&$count): void {
+            ++$count;
+        }));
+        $this->assertSame(3, $count);
+    }
+}
