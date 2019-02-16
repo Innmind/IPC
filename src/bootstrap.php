05c74e20839dc5818975450e79b2633d281274f4
Baptiste Langlade <baptouuuu@gmail.com>
2019-02-16 10:44:31 +0100

    use a sub folder as default sockets folder so no other files will exist

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -4,11 +4,14 @@ declare(strict_types = 1);
 namespace Innmind\IPC;
 
 use Innmind\OperatingSystem\OperatingSystem;
-use Innmind\Url\PathInterface;
+use Innmind\Url\{
+    PathInterface,
+    Path,
+};
 
 function bootstrap(OperatingSystem $os, PathInterface $sockets = null): IPC
 {
-    $sockets = $sockets ?? $os->status()->tmp();
+    $sockets = $sockets ?? new Path("{$os->status()->tmp()}/innmind/ipc");
 
     return new IPC\Unix(
         $os->sockets(),
