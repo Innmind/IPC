8a4cf9335fac8bb118ff536aba677dd6720f283e
Baptiste Langlade <baptiste.langlade@hey.com>
2026-03-14 15:49:07 +0100

    add server fixture

---
+++ fixtures/server.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types = 1);
+
+require __DIR__.'/../vendor/autoload.php';
+
+use Innmind\IPC\{
+    IPC,
+    Process,
+    Message,
+};
+use Innmind\OperatingSystem\OperatingSystem;
+use Innmind\Url\Path;
+use Innmind\MediaType\{
+    MediaType,
+    TopLevel,
+};
+use Innmind\Immutable\Str;
+
+$os = OperatingSystem::new();
+$_ = IPC::of(
+    $os,
+    $os->status()->tmp()->resolve(Path::of('innnmind/ipc/')),
+)
+    ->serve(Process\Name::of('server'))
+    ->with(static function($message, $continuation) {
+        \sleep((int) $message->content()->toString());
+
+        return $continuation->respond(Message::of(
+            MediaType::from(TopLevel::text, 'plain'),
+            Str::of('ack'),
+        ));
+    })
+    ->unwrap();
