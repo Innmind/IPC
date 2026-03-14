e86a43460d5368f62e20201c58f4ce7b8baef295
Baptiste Langlade <baptiste.langlade@hey.com>
2026-03-14 19:20:05 +0100

    test defautl folding

--- proofs/server.php
+++ proofs/server.php
@@ -15,12 +15,61 @@ use Innmind\MediaType\{
 use Innmind\Url\Path;
 use Innmind\Immutable\{
     Str,
+    SideEffect,
     Monoid\Concat,
 };
 use Innmind\BlackBox\Set;
 
 return static function() {
     $os = OperatingSystem::new();
+
+    yield test(
+        'Server default value folding',
+        static function($assert) use ($os) {
+            $process = $os
+                ->control()
+                ->processes()
+                ->execute(
+                    Command::foreground('sleep 2 && php fixtures/client.php')
+                        ->withArgument('fixtures/client.php')
+                        ->withEnvironment('TMPDIR', $os->status()->tmp()->toString())
+                        ->withEnvironment('PATH', $_SERVER['PATH'])
+                        ->withWorkingDirectory(Path::of(__DIR__.'/../')),
+                )
+                ->unwrap();
+
+            $result = IPC::of(
+                $os,
+                $os->status()->tmp()->resolve(Path::of('innnmind/ipc/')),
+            )
+                ->serve(Process\Name::of('server'))
+                ->monitor(
+                    static fn($result, $continuation) => $continuation->finish(),
+                )
+                ->with(
+                    static fn($message, $continuation) => $continuation
+                        ->respond(Message::of(
+                            MediaType::from(TopLevel::text, 'plain'),
+                            Str::of('some output'),
+                        ))
+                        ->finish(),
+                )
+                ->match(
+                    static fn($result) => $result,
+                    static fn() => null,
+                );
+
+            $assert->same(SideEffect::identity, $result);
+            $assert->same(
+                'some output',
+                $process
+                    ->output()
+                    ->map(static fn($chunk) => $chunk->data())
+                    ->fold(Concat::monoid)
+                    ->toString(),
+            );
+        },
+    );
 
     yield proof(
         'Server wait for client',
